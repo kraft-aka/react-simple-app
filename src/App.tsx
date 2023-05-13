197981c1e4e2a50972cf868049e231dfe16e3aa4
kubat <kubat.aiupov@gmail.com>
2023-05-13 22:02:49 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ListGroup from './Components/ListGroup'
+
+export default function App() {
+  return (
+    <div>
+      <ListGroup />
+    </div>
+  )
+}
---
+++ src/Message.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Message() {
+  const name = "Kubat";
+
+  if (name) return <h1>Hello {name}</h1>;
+}
