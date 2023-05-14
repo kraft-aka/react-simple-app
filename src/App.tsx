844667eeb4d0504328f0cde196382074dc585db8
kubat <kubat.aiupov@gmail.com>
2023-05-14 21:54:47 +0200

    init alert, button components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ListGroup from './Components/ListGroup'
+import Alert from './Components/Alert';
+import Button from './Components/Button';
 
 export default function App() {
+  let items = ["NY", "London", "Frankfurt", "Tokyo"];
+  const [alertVisible, setAlertVisible] = useState(false)
+  const handleSelectItem = (item: string) => {
+    console.log(item)
+  }
   return (
-    <div>
-      <ListGroup />
-    </div>
+    <>
+      {alertVisible && <Alert onClose={()=>setAlertVisible(false)}>Hello World!</Alert>}
+      <Button onClick={()=> setAlertVisible(true)}>My Button</Button>
+      <ListGroup items={items} heading='Cities' onSelectItem={handleSelectItem}/>
+    </>
   )
 }
